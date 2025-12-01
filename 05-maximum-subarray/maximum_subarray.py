a6ab3429bdad8ff311d0fc27da71f1fdc622505e
ariana0505 <penaariana075@gmail.com>
2025-12-01 00:24:43 -0500

    fix

--- 05-maximum-subarray/maximum_subarray.py
+++ 05-maximum-subarray/maximum_subarray.py
@@ -1,8 +1,17 @@
 nums = [-2,1,-3,4,-1,2,1,-5,4]
+
 maxSub = nums[0]
 sumaActual = 0
-for i in nums:
-    if nums[i] < 0:
-        sumaActual = 0
-    sumaActual += i 
+
+for i in range(len(nums)):
+    # Sumamos el valor actual
+    sumaActual += nums[i]
+    
+    # Actualizamos el máximo
     maxSub = max(maxSub, sumaActual)
+    
+    # Si la suma acumulada cae debajo de 0, la reiniciamos
+    if sumaActual < 0:
+        sumaActual = 0
+
+print(maxSub)
