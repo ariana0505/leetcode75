7e9d9e7d0c834db4709adf04a748b5e30a8715c3
ariana0505 <penaariana075@gmail.com>
2025-12-21 18:24:54 -0500

    recordar

--- 09-3Sum/3sum.py
+++ 09-3Sum/3sum.py
@@ -1,19 +1,19 @@
-nums = [1,4,3,2,-5,-4,-8,6]
-res = []
+nums = [-1,0,1,2,-1,-4]
+answer = []
 nums.sort()
-for i , a in enumerate(nums):
-    if i>0 and a == nums[i-1]:
+for i, a in enumerate(nums):
+    if i> 0 and a == nums[i-1]:
         continue
-    l , r = i + 1, len(nums)-1
-    while l < r:
-        suma3 = a + nums[l]+ nums[r]
-        if suma3 < 0:
-            l += 1
-        elif suma3 > 0:
-            r -= 1
+    l, r = i+ 1, len(nums)-1
+    while l< r:
+        suma3 = a + nums[l] + nums[r]
+        if 0 < suma3:
+            r = r - 1 
+        elif 0> suma3:
+            l = l +1 
         else:
-            res.append([a,nums[l],nums[r]])
+            answer.append([a, nums[l], nums[r] ])
             l += 1
-            while nums[l] == nums[l-1] and l < r:
-                l += 1
-print(res)
+            while nums[l] == nums[l-1] and l<r:
+                l+=1
+print(answer)
