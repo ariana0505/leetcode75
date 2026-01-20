ed1602a59f7200cea5c0eaa12a5087d5f3d2ae9f
ariana0505 <penaariana075@gmail.com>
2026-01-20 00:05:37 -0500

    add mejor entendimiento

--- 17-longest-increasing-subsequence/longest_increasing_subsequence.py
+++ 17-longest-increasing-subsequence/longest_increasing_subsequence.py
@@ -1,21 +1,13 @@
-# Lista de números de ejemplo
-nums = [10, 9, 2, 5, 3, 7, 101, 18]
 
-# Creamos la lista LIS con valor inicial 1 en cada posición
-# (cada número por sí solo es una subsecuencia de longitud 1)
-LIS = [1] * len(nums)
+# bytequest
 
-# Recorremos la lista desde el final hacia el inicio
-for i in range(len(nums) - 1, -1, -1):
+def lengthOfLIS(nums):
+    n = len(nums)
+    dp = [1] * n
 
-    # Recorremos los elementos que están a la derecha de i
-    for j in range(i + 1, len(nums)):
+    for i in range(n):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
 
-        # Si el número en i es menor que el número en j
-        if nums[i] < nums[j]:
-
-            # Actualizamos el mejor valor posible para LIS[i]
-            LIS[i] = max(LIS[i], 1 + LIS[j])
-
-# Mostramos la longitud de la subsecuencia creciente más larga
-print(max(LIS))
+    return max(dp)
