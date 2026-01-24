affa825dcc218b10ca7cc2626b09d83f09717cd2
ariana0505 <penaariana075@gmail.com>
2026-01-24 15:21:32 -0500

    add new problem

---
+++ 49-longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.py
@@ -0,0 +1,12 @@
+s = "abcabcbb"
+l = 0
+char_set = set()
+max_len = 0
+
+for r in range(len(s)):
+    while s[r] in char_set:
+        char_set.remove(s[l])
+        l += 1
+
+    char_set.add(s[r])
+    max_len = max(max_len, r - l + 1)
